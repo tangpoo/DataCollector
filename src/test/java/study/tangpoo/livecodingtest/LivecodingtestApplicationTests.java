951ae19724cf8322f2014ea7f826b72a04aaa522
tangpoo <lhinav@naver.com>
2024-05-20 10:07:22 +0900

    first commit

---
+++ src/test/java/study/tangpoo/livecodingtest/LivecodingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package study.tangpoo.livecodingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivecodingtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
