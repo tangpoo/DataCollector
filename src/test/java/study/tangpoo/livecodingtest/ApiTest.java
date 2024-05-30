cfa85513779577adc2e431c6f7e115c2b0a2f1fb
tangpoo <lhinav3@gmail.com>
2024-05-31 00:09:10 +0900

    :white_check_mark: Test 전용 H2 DB 적용

--- src/test/java/study/tangpoo/livecodingtest/ApiTest.java
+++ src/test/java/study/tangpoo/livecodingtest/ApiTest.java
@@ -3,10 +3,15 @@ package study.tangpoo.livecodingtest;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2, replace = AutoConfigureTestDatabase.Replace.ANY)
+@TestPropertySource("classpath:application-test.properties")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApiTest {
 
