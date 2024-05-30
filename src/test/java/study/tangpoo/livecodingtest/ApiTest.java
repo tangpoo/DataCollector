38a929b512dcc7642f0e328309fae57c8a34d9c7
tangpoo <lhinav3@gmail.com>
2024-05-30 19:13:17 +0900

    :white_check_mark: ApiTest base 파일 생성

---
+++ src/test/java/study/tangpoo/livecodingtest/ApiTest.java
@@ -0,0 +1,27 @@
+package study.tangpoo.livecodingtest;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        if(RestAssured.port == RestAssured.UNDEFINED_PORT){
+            RestAssured.port = port;
+            databaseCleanup.afterPropertiesSet();
+        }
+        RestAssured.port = port;
+    }
+}
